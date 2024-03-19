12eeeda4bc3e8e4b63b388e1741fa15cc8ce73ff
thabumon <52034689+thabumon@users.noreply.github.com>
2024-03-19 11:11:09 +0530

    edtz

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-"use strict";
-var gulp = require("gulp");
-var requireDir = require("require-dir");
-requireDir("gulp-tasks");
+'use strict'
+var gulp = require('gulp');
+var requireDir = require('require-dir');
+requireDir('gulp-tasks');
+
 
 gulp.paths = {
-  dist: "dist",
+    dist: 'dist',
 };
 
 var paths = gulp.paths;
 
-gulp.task("default", gulp.series("serve"));
+gulp.task('default', gulp.series('serve'));
