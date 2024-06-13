c9bfdfcc2d9c157ab5b703e5f512c301a4d13007
Mohammed Sabith E <108073559+Mohammed-Sabith-E@users.noreply.github.com>
2024-06-13 18:03:19 +0530

    Update global.js

--- globals/global.js
+++ globals/global.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'https://wbm-server.onrender.com';
+export const BACKEND_URL = 'https://wbm-server-5ga3.onrender.com';
 export const USER_ID = sessionStorage.getItem('uid');;
